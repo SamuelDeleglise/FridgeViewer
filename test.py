c512b7627b810e6b5d8eef0e0deb153c2b79c47e
Yifan Li <antoineli1994@gmail.com>
2019-05-21 10:02:07 +0200

    Col and Row

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
 import pandas as pd
@@ -10,10 +9,12 @@ import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=[external_stylesheets])
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#########################################################
-# Acuquisition of data
+#app = dash.Dash(__name__)
+
+######################################################
+# Receive the data
 df = pd.read_csv(
     'https://gist.githubusercontent.com/chriddyp/'
     'cb5392c35661370d95f300086accea51/raw/'
@@ -21,17 +22,18 @@ df = pd.read_csv(
     'indicators.csv')
 available_indicators = df['Indicator Name'].unique()
 
+
+
+
 CHANNELS = ['temp1', 'temp2']
 TIMES1 = np.linspace(0,15, 200)
 DATA1 = np.cos(TIMES1)
 TIMES2 = np.linspace(0,15, 200)
 DATA2 = np.sin(TIMES2)
 
-#########################################################
-# Layout of page
+
 
 app.layout = html.Div([
-    
     # Banner display
     html.Div([
         # Title
@@ -39,78 +41,91 @@ app.layout = html.Div([
             id='title',
             style={
                     'position': 'relative',
-                    'top': '5px',
-                    'left': '15px',
+                    'top': '0px',
+                    'left': '0px',
                     'font-family': 'Dosis',
                     'display': 'inline',
-                    'font-size': '4.0rem',
+                    'font-size': '6.0rem',
                     'color': '#4D637F'
             }
         ),
         # Logo
         html.Img(src="https://tel.archives-ouvertes.fr/LKB-THESE/public/Logo_LKB.png",
-            style={ 'top': '5px',
+            style={ 'top': '0px',
                     'height': '90px',
                     'float': 'right',
                     'position': 'relative',
-                    'right': '15px'
+                    'right': '0px'
             },
         )
     ],className="banner", style={'position': 'relative', 'right': '15px'}
     ),
     
     # Channel selection
-    html.Div(className='tem_channel',children=[
-        # Head
+    html.Div([
         html.Div([
-            html.H2('Select the signal channel'),
-           # head style
-        ], style={'margin-left': '10px','margin-top': '10px'}
+            html.P('Select the signal channel'),
+        ], style={'margin-left': '10px'}
         ),
         # Channel selection dropdown
-        dcc.Dropdown(id='channels_dropdown',
-            options=[{'label': i, 'value': i} for i in CHANNELS],
+        dcc.Dropdown(id='channel',
+            options=[
+                {'label': 'Tem RnO2', 'value': 'tem_rno2'},
+                {'label': 'Tem 1Kpot', 'value': 'tem_kpot'},
+                {'label': 'Tem Helium 3', 'value': 'temp_he'}
+            ],
             # defaut selections
-            value=CHANNELS,
+            value=['tem_rno2', 'tem_kpot','temp_he'],
             multi=True,
         )
-    # channel selection style
-    ], style = {'left':'15px', 'right':'15px'}
+    ],className='tem_channel' 
     ),
-    
-    # Body
-    dbc.Row(className="Row", children=[
-        dbc.Col([        
-            dcc.Graph(id='indicator-graphic',)
-              # temp block style
-            ], className='graph col', style={ 'height':'700px', 'float': 'left', 'display': 'inline-block'}
-        ),
-        dbc.Col([
-            html.Div([
-                dcc.DatePickerRange(id='date_range')
-            # date range style
-            ],style={'width': '100%', 'display': 'inline-block'}
-            ),
-          # selection block style
-        ], className='selection col', style={'width': '25%', 'height':'700px', 'float': 'right', 'padding': 20, 'margin': 5, 'borderRadius': 5, 'border': 'thin lightgrey solid'}
-        )
-    ]),
-        
-        
-    # Markdown Description
-    html.Div(className='text',children=[
-        html.Div(children=dcc.Markdown(),
-                style={'width': '80%','margin': '30px auto'}
-        )
-    ])
-])
 
-#########################################################
-# Callback and update
+    # Tabs and Figure
+    html.Div([
+        # Tabs
+        dcc.Tabs(id="tabs", 
+            value='tab-1', 
+            children=[
+                dcc.Tab(label='Tab one', value='tab-1'),
+                dcc.Tab(label='Tab two', value='tab-2'),
+            ],style={'width': '250px'}
+            ),
+        # Tabs content
+        html.Div(
+            # Checklist of temperature 
+            html.Div([
+            dcc.Checklist(
+                id='channels',
+                options=[{'label': i, 'value': i} for i in CHANNELS],
+                values=[]
+            )],style={'width': '48%'}
+            ),
+            # Date Picker
+            html.Div([
+            dcc.DatePickerRange(
+                id='my-date-picker-range',
+                min_date_allowed=dt(1995, 8, 5),
+                max_date_allowed=dt(2017, 9, 19),
+                initial_visible_month=dt(2017, 8, 5),
+                end_date=dt(2017, 8, 25)
+            ),
+            # Print picked date
+            html.Div(id='output-container-date-picker-range')
+            ],style={'display': 'inline-block'})
+            ),
+        # Real-time Graph 
+        html.Div([
+        dcc.Graph(id='indicator-graphic')
+        ],style={'width': '100%', 'height':'600px', 'float': 'right', 'display': 'inline-block'}
+        ),
+    ]),
+], className='container'
+)
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    [Input('channels_dropdown', 'value'),
+    [Input('channels', 'values'),
      Input('date_range', 'end_date')],
      [State('date_range', 'start_date')])
 def update_graph(channels, end_date, start_date):
